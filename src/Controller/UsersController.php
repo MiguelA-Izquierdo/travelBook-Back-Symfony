b4246b972bce0d3948319f858ebbd163ef1bd06a
Miguel Angel Izquierdo <izquierdomiguela@gmail.com>
2023-11-02 17:32:21 +0100

    Create DB & Entity User & Entity ImgData

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,22 +4,24 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
 class UsersController extends AbstractController{
 
 
   /**
-     * @Route("/users/list", name="users_list")
-     */
+   * @Route("/users/list", name="users_list")
+   */
 
-public function getAll() {
+public function getAll(Request $request) {
     $response = new JsonResponse();
-    // $response->setContent('<div>Lista de usuarios</div>');
+    $userName= $request->get('userName', 'Unknow');
     $response->setData([
         'success' => true,
         'data' => [
             [
                 'id' => 1,
-                'userName' => 'SrIzquierdo'
+                'userName' => $userName
             ]
         ]
     ]);
---
+++ src/Entity/User.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $userName;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $lastName;
+
+    /**
+     * @ORM\OneToOne(targetEntity="ImgData")
+     * @ORM\JoinColumn(name="avatar_id", referencedColumnName="id")
+     */
+    private $avatar;
+
+    // Getters and setters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setUserName($userName) {
+        $this->userName = $userName;
+    }
+
+    public function getUserName() {
+        return $this->userName;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setFirstName($firstName) {
+        $this->firstName = $firstName;
+    }
+
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    public function setLastName($lastName) {
+        $this->lastName = $lastName;
+    }
+
+    public function getLastName() {
+        return $this->lastName;
+    }
+
+    public function setAvatar($avatar) {
+        $this->avatar = $avatar;
+    }
+
+    public function getAvatar() {
+        return $this->avatar;
+    }
+}
